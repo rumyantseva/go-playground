a51426b2c6f56392ea9935ed3f7d9db78ef6094b
Elena Grahovac <elena@grahovac.me>
2018-01-09 11:03:43 +0100

    logrus + investigation results

--- glog-examples/main.go
+++ glog-examples/main.go
@@ -5,13 +5,23 @@ import (
 	"fmt"
 
 	"github.com/golang/glog"
+	"github.com/sirupsen/logrus"
 )
 
 var (
-	exit = flag.Bool("exit", false, "If true, will do glog.Exit (or glog.Fatal otherwise)")
+	exit      = flag.Bool("exit", false, "If true, will do glog.Exit (or glog.Fatal otherwise)")
+	useLogrus = flag.Bool("logrus", false, "If true, will use logrus (or glog otherwise)")
 )
 
-// glog.Fatal vs glog.Exit
+// This application shows how defer work with logger.Fatal or logger.Error.
+// Explanation: in case of logger.Fatal/logger.Error defer will not be done
+// (application will os.Exit immediately).
+
+// How to run this app:
+// try logrus.Exit: go run main.go --logrus --exit
+// try logrus.Fatal: go run main.go --logrus
+// try glog.Exit: go run main.go --exit
+// try glog.Fatal: go run main.go
 func main() {
 	flag.Parse()
 	myfunc()
@@ -22,9 +32,17 @@ func myfunc() {
 		fmt.Println("Yeah! I was deferred.")
 	}()
 
-	if *exit {
-		glog.Exit("Trying glog.Exit...")
+	if *useLogrus {
+		if *exit {
+			logrus.Exit(255)
+		} else {
+			logrus.Fatal("Trying glog.Exit...")
+		}
 	} else {
-		glog.Fatal("Trying glog.Fatal...")
+		if *exit {
+			glog.Exit("Trying glog.Exit...")
+		} else {
+			glog.Fatal("Trying glog.Fatal...")
+		}
 	}
 }
